a1d78f5c8680f6b66a07245960753aa0a357b4f9
Udit Karode <udit.karode@gmail.com>
2021-02-20 13:55:21 +0530

    polybar: dwm: bump

--- polybar/polybar_dwm/pastatus.sh
+++ polybar/polybar_dwm/pastatus.sh
@@ -5,7 +5,7 @@ SOURCE="$(pactl info | grep -i 'source' | sed 's/Default Source: //')"
 SPEAKER_SINK="alsa_output.pci-0000_00_1f.3.analog-stereo"
 HDMI_SINK="alsa_output.pci-0000_00_1f.3.hdmi-stereo-extra2"
 
-AUDIO_DEVICE="["
+AUDIO_DEVICE=""
 
 if [ "$SINK" = "$SPEAKER_SINK" ]; then
   AUDIO_DEVICE="${AUDIO_DEVICE}SPKR"
@@ -16,7 +16,5 @@ if [ -z "$(echo "$SOURCE" | grep 'monitor')" ]; then
   # the input device is a microphone of some sort
   AUDIO_DEVICE="${AUDIO_DEVICE}+MIC"
 fi
-
-AUDIO_DEVICE="${AUDIO_DEVICE}]"
 
 echo "$AUDIO_DEVICE"
--- polybar/polybar_dwm/scripts/pywal.sh
+++ polybar/polybar_dwm/scripts/pywal.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/material/colors.ini"
-RFILE="$HOME/.config/polybar/material/scripts/rofi/colors.rasi"
+PFILE="$HOME/.config/polybar/colors.ini"
+RFILE="$HOME/.config/polybar/scripts/rofi/colors.rasi"
 
 # Get colors
 pywal_get() {
